1f2c37206231395e35d5bb28428700ead7d148d3
JulianDavidG07 <julygsaenz1995@hotmail.com>
2023-12-06 17:45:22 -0500

    [ADD] Messages

--- content-happy.js
+++ content-happy.js
@@ -2,12 +2,31 @@
 var lastHoveredButton = "btn-primary"; // Agrega esta línea
 
 var mensajesHappy = [
-  "¡Hola! Este es un mensaje aleatorio. .)",
-  "¿Sabías que los mensajes cambian cada vez?",
-  "¡Espero que tengas un gran día!",
-  "Juan es crazy",
-  "Este es otro mensaje interesante.",
-  "¡Diviértete explorando!",
-  "Puedes agregar más mensajes al array según tus necesidades.",
+    "Que cada paso que des esté guiado por la luz divina. 🚶‍♂️💡",
+    "Que la paz de Dios te acompañe hoy y siempre. 💖🌟",
+    "Cada día es una página en blanco. Escribe una historia increíble llena de risas, amor y logros. ¡Hoy es tu día para brillar! 💫✨",
+    "Recuerda: cada nube tiene un borde plateado. ¡Que las pequeñas alegrías iluminen tu día y te hagan sonreír! 🌈😁",
+    "Eres como un rayo de sol en un día nublado. Tu positividad ilumina y calienta los corazones a tu alrededor. ☀️💛",
+    "La vida es corta, así que come el postre primero, ríe demasiado y nunca dejes de ser una chispa de felicidad. 🍰😂",
+    "No cuentes los días, haz que los días cuenten. ¡Que hoy sea tan brillante y asombroso como tú eres! 🚀🌟",
+    "La felicidad es una elección. Elige ser feliz hoy y deja que esa elección ilumine tu día y el de los demás. 😃💖",
+    "Si la belleza fuera tiempo, ¡tú serías la eternidad! ⏳💖",
+    "Eres como un 🌈 en un día lluvioso, llenando la vida de colores y alegría.",
+    "¿Eres una 🌺? Porque cada vez que te veo, florece la felicidad en mi corazón.",
+    "Eres mi ☀️ en los días grises, siempre trayendo calidez y luz a mi vida.",
+    "Si fueras un 🎨, serías la obra maestra más hermosa que mis ojos han visto.",
+    "¿Eres un 🚀? Porque desde que llegaste, mi corazón está en una emocionante travesía.",
+    "Eres como un 🍭, siempre endulzando mis días con tu ternura y alegría.",
+    "Si la perfección fuera un 💎, tú serías la joya más preciosa.",
+    "Eres como una 🎶 encantadora que nunca deja de sonar en mi corazón.",
+    "Eres como un 📚 de cuentos de hadas, cada página de nuestra historia es mágica.",
+    "Si cada pensamiento tuyo fuera una 🎈, llenarías el cielo de colores.",
+    "Si la vida fuera un escenario, tú serías la ⭐ principal.",
+    "Eres el sueño del que no quiero despertar, la razón de mi sonrisa. 😊✨",
+    "Si fueras una estrella, iluminarías mi cielo incluso en la noche más oscura. 🌟🌌",
+    "Eres mi inspiración diaria, el motivo por el cual cada día es especial. 🌈💫",
+    "Si la felicidad fuera un lugar, sería a tu lado. 🏡❤️",
+    
+
 ];
 
