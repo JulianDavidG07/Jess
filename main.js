311844f6bf0dc739b3dfd90ef1321d9ff8a8561b
JulianDavidG07 <julygsaenz1995@hotmail.com>
2024-02-25 13:29:11 -0500

    [ADD] Counter

--- main.js
+++ main.js
@@ -72,3 +72,46 @@ controlBtn.addEventListener("click", playPause);
 track.addEventListener("ended", function () {
   controlBtn.className = "play";
 });
+
+
+/********** countdown ************* */
+function updateTimer() {
+  // Establecer la fecha de nacimiento y la fecha actual
+  var past = new Date("2023-12-07T00:00:00");
+  var now = new Date();
+
+  // Calcular la diferencia en milisegundos
+  var diff = now - past;
+
+  // Convertir la diferencia de milisegundos a años, meses, días, horas, minutos y segundos
+  var years = now.getFullYear() - past.getFullYear();
+  var months = now.getMonth() - past.getMonth();
+  var days = now.getDate() - past.getDate();
+  var hours = now.getHours() - past.getHours();
+  var minutes = now.getMinutes() - past.getMinutes();
+  var seconds = now.getSeconds() - past.getSeconds();
+
+  // Ajustar los valores si es necesario (para garantizar que sean valores positivos)
+  if (seconds < 0) { minutes--; seconds += 60; }
+  if (minutes < 0) { hours--; minutes += 60; }
+  if (hours < 0) { days--; hours += 24; }
+  if (days < 0) {
+    var daysInLastMonth = new Date(now.getFullYear(), now.getMonth(), 0).getDate();
+    months--;
+    days += daysInLastMonth;
+  }
+  if (months < 0) { years--; months += 12; }
+
+  // Actualizar el elemento con el tiempo transcurrido
+  document.getElementById("timer").innerHTML =
+    '<div>' + years + '<span>Año(s)</span></div>' +
+    '<div>' + months + '<span>Mese(s)</span></div>' +
+    '<div>' + days + '<span>Dia(s)</span></div>' +
+    '<div>' + hours + '<span>Hora(s)</span></div>' +
+    '<div>' + minutes + '<span>Minuto(s)</span></div>' +
+    '<div>' + seconds + '<span>Segundo(s)</span></div>' +
+    '<div class="mini-text"><span>Bendecidos<br>¡A tu lado! <3</span></div>';
+}
+
+// Llamar a la función updateTimer cada segundo
+setInterval(updateTimer, 1000);
