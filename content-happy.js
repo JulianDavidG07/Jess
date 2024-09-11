348d1492457cf93fcef433cc74dc123a0e8838cf
Julian David Gaitan Saenz <julygsaenz1995@hotmail.com>
2024-09-10 23:47:19 -0500

    add snow

--- content-happy.js
+++ content-happy.js
@@ -1,120 +1,111 @@
-var d = document,
-    w = window,
-    wWidth = w.innerWidth,
-    wHeight = w.innerHeight,
-    credit = document.querySelector('.credit > a'),
-    particles = document.querySelector('.particles'),
-    particleCount = 0,
-    sizes = [15, 20, 25, 35, 45],
-    colors = [
-      '#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5',
-      '#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4CAF50',
-      '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800',
-      '#FF5722', '#795548', '#9E9E9E', '#607D8B', '#777777'
-    ],
-    mouseX = w.innerWidth / 2, mouseY = w.innerHeight / 2;
+(function() {
+  var snowflakes = [],
+      moveAngle = 0,
+      animationInterval;
 
-function updateParticleCount() {
-  document.querySelector('.particle-count > .number').textContent = particleCount;
-}
-
-w.addEventListener('resize', function() {
-  wWidth = w.innerWidth;
-  wHeight = w.innerHeight;
-});
-
-d.addEventListener('mousemove', function(event) {
-  event.preventDefault();
-  event.stopPropagation();
-  mouseX = event.clientX;
-  mouseY = event.clientY;
-});
-
-d.addEventListener('touchmove', function(event) {
-  event.preventDefault();
-  event.stopPropagation();
-  if (event.touches && event.touches.length > 0) {
-    mouseX = event.touches[0].clientX;
-    mouseY = event.touches[0].clientY;
+  /**
+   * Generates a random number between the min and max (inclusive).
+   * @method getRandomNumber
+   * @param {Number} min
+   * @param {Number} max
+   * @return {Number}
+   */
+  function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-});
 
-d.addEventListener('mousedown', function(event) {
-  if (event.target === credit) {
-    return;
+  /**
+   * Creates a new snowflake div and returns it.
+   * @method createSnowflake
+   * @return {HTMLElement}
+   */
+  function createSnowflake() {
+    var el = document.createElement('div'),
+        style = el.style;
+
+    style.borderRadius = '100%';
+    style.border = getRandomNumber(1, 4) + 'px solid white';
+    style.position = 'fixed';
+    style.zIndex = '999999';
+    style.boxShadow = '0 0 2px rgba(255,255,255,0.8)';
+    style.top = getRandomNumber(0, window.innerHeight) + 'px';
+    style.left = getRandomNumber(0, window.innerWidth) + 'px';
+
+    return el;
   }
-  var timer = setInterval(function() {
-    d.addEventListener('mouseup', function() {
-      clearInterval(timer);
-    });
-    d.addEventListener('mouseleave', function() {
-      clearInterval(timer);
-    });
-    createParticle(event);
-  }, 1000 / 60);
-});
 
-d.addEventListener('touchstart', function(event) {
-  if (event.target === credit) {
-    return;
-  }
-  var timer = setInterval(function() {
-    d.addEventListener('touchend', function() {
-      clearInterval(timer);
-    });
-    d.addEventListener('touchcancel', function() {
-      clearInterval(timer);
-    });
-    createParticle(event);
-  }, 1000 / 60);
-});
+  /**
+   * Calls the moveSnowflake method for each of the snowflakes in the cache.
+   * @method moveSnowflakes
+   * @return {Void}
+   */
+  function moveSnowflakes() {
+    var l = snowflakes.length,
+        i;
 
-function createParticle(event) {
-  var particle = document.createElement('div');
-  particle.className = 'particle';
+    moveAngle += 0.01;
 
-  var size = sizes[Math.floor(Math.random() * sizes.length)],
-      color = colors[Math.floor(Math.random() * colors.length)],
-      negative = size / 2,
-      speedHorz = Math.random() * 10,
-      speedUp = Math.random() * 25,
-      spinVal = 360 * Math.random(),
-      spinSpeed = ((36 * Math.random())) * (Math.random() <= 0.5 ? -1 : 1),
-      otime,
-      time = otime = (1 + (0.5 * Math.random())) * 1000,
-      top = mouseY - negative,
-      left = mouseX - negative,
-      direction = Math.random() <= 0.5 ? -1 : 1,
-      life = 10;
-
-  particle.style.height = size + 'px';
-  particle.style.width = size + 'px';
-  particle.style.top = top + 'px';
-  particle.style.left = left + 'px';
-  particle.style.background = color;
-  particle.style.transform = 'rotate(' + spinVal + 'deg)';
-
-  particles.appendChild(particle);
-  particleCount++;
-  updateParticleCount();
-
-  var particleTimer = setInterval(function() {
-    time -= life;
-    left -= speedHorz * direction;
-    top -= speedUp;
-    speedUp = Math.min(size, speedUp - 1);
-    spinVal += spinSpeed;
-
-    particle.style.top = top + 'px';
-    particle.style.left = left + 'px';
-    particle.style.opacity = ((time / otime) / 2) + 0.25;
-    particle.style.transform = 'rotate(' + spinVal + 'deg)';
-
-    if (time <= 0 || left <= -size || left >= wWidth + size || top >= wHeight + size) {
-      particles.removeChild(particle);
-      particleCount--;
-      updateParticleCount();
-      clearInterval(particleTimer);
+    for (i=0; i<l; i++) {
+      moveSnowflake(snowflakes[i]);
     }
-  }, 1000 / 60);
-}
+  }
+
+  /**
+   * Moves an individual snowflake element using some simple math.
+   * @method moveSnowflake
+   * @param {HTMLElement} el
+   * @return {Void}
+   */
+  function moveSnowflake(el) {
+    var style = el.style,
+        height = window.innerHeight,
+        radius,
+        top;
+
+    radius = parseInt(style.border, 10);
+
+    top = parseInt(style.top, 10);
+    top += Math.cos(moveAngle) + 1 + radius/2;
+
+    if (top > height) {
+      resetSnowflake(el);
+    } else {
+      style.top = top + 'px';
+    }
+  }
+
+  /**
+   * Puts the snowflake back at the top in a random horizontal start position.
+   * @method resetSnowflake
+   * @param {HTMLElement} el
+   * @return {Void}
+   */
+  function resetSnowflake(el) {
+    var style = el.style;
+
+    style.top = '0px';
+    style.left = getRandomNumber(0, window.innerWidth) + 'px';
+  }
+
+  /**
+   * The kick-off method. Asks how many snowflakes to make and then makes them!
+   * @method setup
+   * @return {Void}
+   */
+  function setup() {
+    var number = 100,
+        particle,
+        i;
+
+    // Setup snow particles
+    for (i=0; i<number; i++) {
+      particle = snowflakes[i] = createSnowflake();
+      document.body.appendChild(particle);
+    }
+
+    // Set animation intervals
+    animationInterval = setInterval(moveSnowflakes, 33);
+  }
+
+  setup();
+}());
